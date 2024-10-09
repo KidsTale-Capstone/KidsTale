a6191f0117c47c0a09944d50f71e18d523f5d9fb
Woojung Paek <woojungpaek@Woojungs-MacBook-Pro-2.local>
2024-10-09 19:45:18 +0900

    241009 v1.0 WJ

--- front/script.js
+++ front/script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 if (response.ok) {
                     alert(result.message || '회원가입이 완료되었습니다.');
-                    window.location.href = '/login.html';  // 성공 시 리다이렉트
+                    window.location.href = 'login.html';  // 성공 시 리다이렉트
                 } else {
                     alert(result.message || '회원가입에 실패했습니다.');
                 }
@@ -71,3 +71,39 @@ document.getElementById("progress-percentage").innerText = `${progressPercentage
 
 // 고양이 이미지 위치 업데이트 (진행도에 맞게, 바 끝에 고정)
 document.getElementById("cat-img").style.left = `calc(${progressPercentage}% - 25px)`;
+
+
+
+// 1. 그림 업로드 페이지
+// 사용자가 파일을 선택했을 때 이미지 미리보기
+function previewImage(event) {
+    const input = event.target;
+    const uploadLabel = document.getElementById('upload-label'); // 업로드 문구 가져오기
+    const previewBox = document.getElementById('preview-box');
+    const uploadBox = document.querySelector('.upload-box'); // 업로드 박스 가져오기
+
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            // "그림을 업로드 해주세요" 문구 숨기기
+            uploadLabel.style.display = 'none';
+            
+            // 배경색을 하얀색으로 변경
+            uploadBox.style.backgroundColor = 'white';
+            
+            // 이미지를 보여줌
+            const img = document.createElement('img');
+            img.src = e.target.result;
+            previewBox.innerHTML = ''; // 기존 내용 제거
+            previewBox.appendChild(img); // 이미지 추가
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+// 다음 버튼 누르면 다음 페이지로 이동하는 함수
+function goToNextPage() {
+    window.location.href = "select_keywords.html"; // 이동할 페이지 경로
+}
